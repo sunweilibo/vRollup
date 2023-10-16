fab8db07893354fa27fcbb478c3b7e5c17aac4b3
sunweili <sunweili@joyy.com>
2023-10-16 10:42:37 +0800

    refactor: iife

--- src/import/main.js
+++
@@ -1,7 +0,0 @@
-console.log(20)
-let n = Math.random() * 10
-if (n > 3) {
-  import('./tools.js').then((c) => {
-    console.log(c.add(1,2))
-  })
-}
--- src/import/tools.js
+++
@@ -1,8 +0,0 @@
-import _add from 'lodash/add'
-export function add(a,b) {
-  return _add(a,b);
-}
-
-export function sub(a,b) {
-  return a-b
-}
