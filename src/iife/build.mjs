153bde175e3803015c7ce1b7ee3cee46eb76dd11
sunweili <sunweili@joyy.com>
2023-10-16 10:41:51 +0800

    refactor: iife

---
+++ src/iife/build.mjs
@@ -0,0 +1,20 @@
+import {loadConfigFile} from 'rollup/loadConfigFile';
+import path from 'path';
+import {rollup} from 'rollup';
+import { fileURLToPath } from 'url';
+
+const _dirname = fileURLToPath(new URL('.', import.meta.url))
+
+loadConfigFile(path.resolve(_dirname, 'rollup.config.mjs')).then(async ({options, warnings}) => {
+  // "warnings" wraps the default `onwarn` handler passed by the CLI.
+	// This prints all warnings up to this point:
+	console.log(`We currently have ${warnings.count} warnings`);
+
+	// This prints all deferred warnings
+	warnings.flush();
+
+  for (const optionsObj of options) {
+		const bundle = await rollup(optionsObj);
+		await Promise.all(optionsObj.output.map(bundle.write));
+	}
+})
---
+++ src/iife/rollup.config.mjs
@@ -0,0 +1,35 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import clear from 'rollup-plugin-clear'
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+const getAbsPath = (name) => path.resolve(__dirname, name)
+const outputDir = getAbsPath('output')
+// @ts-check
+/** @type {import('rollup/dist/rollup').RollupOptions} */
+const options = {
+  input: path.resolve(__dirname, './tools.js'),
+  external: ['lodash'],
+  output: {
+    dir: outputDir,
+    format: 'iife',
+    name: 'v',
+    globals: {
+      lodash: '_'
+    }
+  },
+  plugins: [
+    clear({
+      targets: [outputDir]
+    }),
+    nodeResolve(),
+    commonjs({
+      strictRequires: true
+    })
+  ]
+}
+
+export default options
